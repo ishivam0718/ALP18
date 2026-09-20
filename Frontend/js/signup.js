e914cd2bb342abf8984e787db5ea95933b402ba6
ishivam0718 <ishivam0718@gmail.com>
2026-09-21 03:22:50 +0530

    Fix HTTPS backend connection

--- Frontend/js/signup.js
+++ Frontend/js/signup.js
@@ -13,7 +13,7 @@ function signup() {
     return;
   }
 
-  fetch("http://alp18-backend.onrender.com/api/signup", {
+  fetch("https://alp18-backend.onrender.com/api/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
